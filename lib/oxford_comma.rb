44c90b2ff963bb8f98e31948a1966f094bd4db0d
Sam Tamburri <tamburrisam@gmail.com>
2020-04-09 18:55:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+array = ["fiddleheads", "okra", "kohlrabi"]
 
+def oxford_comma(array)
+#array.join(" and ")
+array.each do |words|
+  until array == array.last
+    array.join(" and ")
+  end
+end
 end
